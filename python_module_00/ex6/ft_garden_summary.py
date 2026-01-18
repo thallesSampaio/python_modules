5f2065d48a57153bc7c06b4718b699595898006d
thasampa <thallessampaio89@gmail.com>
2026-01-18 15:52:08 -0300

    fixing ex6

--- python_module_00/ex6/ft_garden_summary.py
+++ python_module_00/ex6/ft_garden_summary.py
@@ -1,6 +1,6 @@
 def ft_garden_summary():
     garden_name = input("Enter garden name: ")
-    plants_number = int(input("Enter number of plantes: "))
+    plants_number = input("Enter number of plantes: ")
     print("Garden:", garden_name)
     print("Plants:", plants_number)
     print("Status: Growing well!")
