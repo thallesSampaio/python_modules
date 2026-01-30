549cc353971f31399f4f7ff6d640aa08633369ba
thasampa <thallessampaio89@gmail.com>
2026-01-29 22:25:38 -0300

    fix: flake8 module 00 ex5 recursive

--- python_module_00/ex5/ft_count_harvest_recursive.py
+++ python_module_00/ex5/ft_count_harvest_recursive.py
@@ -1,9 +1,10 @@
 def ft_count_harvest_recursive():
-	days = int(input("Days until harvest: "))
-	def helper(current_day):
-		if current_day > days:
-			print("Harvest time!")
-			return
-		print("Day", current_day)
-		helper(current_day + 1)
-	helper(1)
+    days = int(input("Days until harvest: "))
+
+    def helper(current_day):
+        if current_day > days:
+            print("Harvest time!")
+            return
+        print("Day", current_day)
+        helper(current_day + 1)
+    helper(1)
