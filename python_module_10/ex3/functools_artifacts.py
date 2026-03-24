3bb61b47bea938f5a381b09c252c2e0f650f69e8
thasampa <thallessampaio89@gmail.com>
2026-03-24 16:50:46 -0300

    avz

--- python_module_10/ex3/functools_artifacts.py
+++ python_module_10/ex3/functools_artifacts.py
@@ -4,20 +4,31 @@ from typing import Any
 
 
 def spell_reducer(spells: list[int], operation: str) -> int:
-    ops = {
-        "add": add,
-        "multiply": mul,
-        "max": max,
-        "min": min
-    }
 
-    if operation not in ops:
+    if not isinstance(spells, list):
+        raise TypeError("TypeError Spells must be a list of integers.")
+    if not spells:
+        raise ValueError("ValueError Spells can't be empty.")
+    for s in spells:
+        if not isinstance(s, int):
+            raise TypeError("TypeError Spells must be a list of integers.")
+
+    if operation == "add":
+        return reduce(add, spells)
+    elif operation == "multiply":
+        return reduce(mul, spells)
+    elif operation == "max":
+        return reduce(max, spells)
+    elif operation == "min":
+        return reduce(min, spells)
+    else:
         raise ValueError(f"Unknow operation: '{operation}'.")
-
-    return reduce(ops[operation], spells)
 
 
 def partial_enchanter(base_enchantment: callable) -> dict[str, callable]:
+    if not callable(base_enchantment):
+        raise TypeError("TypeError: base_enchantment must be a callable.")
+
     return {
         "fire_enchant": partial(base_enchantment, power=50, element="fire"),
         "ice_enchant": partial(base_enchantment, power=50, element="ice"),
@@ -26,8 +37,10 @@ def partial_enchanter(base_enchantment: callable) -> dict[str, callable]:
     }
 
 
-@lru_cache(maxsize=None)
+@lru_cache
 def memoized_fibonacci(n: int) -> int:
+    if not isinstance(n, int):
+        raise TypeError(f"TypeError: '{n}' is not an integer.")
     if n < 2:
         return n
     return memoized_fibonacci(n - 1) + memoized_fibonacci(n - 2)
@@ -48,33 +61,47 @@ def spell_dispatcher() -> callable:
 
     @dispatcher.register(list)
     def _(arg: list):
+        if not arg:
+            raise ValueError("List cant be empty.")
         return f"Multi-casting: {', '.join(map(str, arg))}"
 
     return dispatcher
 
 
 if __name__ == "__main__":
-    print("\nTesting spell reducer...")
-    sum = [20, 20, 20, 20, 20]
-    product = [240, 100, 10]
-    max_values = [1, 2, 39, 27, 40, 22]
-    print(f"Sum: {spell_reducer(sum, 'add')}")
-    print(f"Product: {spell_reducer(product, 'multiply')}")
-    print(f"Max: {spell_reducer(max_values, 'max')}")
+    try:
+        print("\nTesting spell reducer...")
+        sum = [20, 20, 20, 20, 20]
+        product = [240, 100, 10]
+        max_values = [1, 2, 39, 27, 40, 22]
+        print(f"Sum: {spell_reducer(sum, 'add')}")
+        print(f"Product: {spell_reducer(product, 'multiply')}")
+        print(f"Max: {spell_reducer(max_values, 'max')}")
+    except Exception as e:
+        print(f"Error: {e}")
 
-    print("\nTesting memoized fibonacci...")
-    print(f"Fib(10): {memoized_fibonacci(10)}")
+    try:
+        print("\nTesting memoized fibonacci...")
+        print(f"Fib(10): {memoized_fibonacci(10)}")
+    except TypeError as e:
+        print(f"Error: {e}")
 
-    print("\nTesting partial enchanter...")
+    try:
+        print("\nTesting partial enchanter...")
 
-    def base_enchantment(target: str, power: int, element: str) -> str:
-        return f"{target} attacked by {element} weapon with {power} power"
-    res = partial_enchanter(base_enchantment)
-    print(res["fire_enchant"]("dragon"))
-    print(res['ice_enchant']('goblins'))
+        def base_enchantment(target: str, power: int, element: str) -> str:
+            return f"{target} attacked by {element} weapon with {power} power"
+        res = partial_enchanter(base_enchantment)
+        print(res["fire_enchant"]("dragon"))
+        print(res['ice_enchant']('goblins'))
+    except TypeError as e:
+        print(f"Error: {e}")
 
-    print("\nTesting spell dispatcher...")
-    dispatch = spell_dispatcher()
-    print(dispatch(100))
-    print(dispatch("Invisibility"))
-    print(dispatch([1, 2, 3]))
+    try:
+        print("\nTesting spell dispatcher...")
+        dispatch = spell_dispatcher()
+        print(dispatch(100))
+        print(dispatch("Invisibility"))
+        print(dispatch([]))
+    except ValueError as e:
+        print(f"\nValueError: {e}")
